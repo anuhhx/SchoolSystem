64f2ad6e0c7aa49a311a8197b2d485b304bd9ae5
Ana Moreno <amoreno5@uncc.edu>
2018-11-22 03:41:31 +0000

    Added add Schools view and new school method

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,19 +1,25 @@
 class SchoolsController < ApplicationController
     def show
         @school=County.find(params[:county_id]).schools.find(params[:id])
+        @county=County.find(params[:county_id])
     end
     
     def new
         @school =County.find(params[:county_id]).schools.new
+        @county=County.find(params[:county_id])
     end
     
     def create
         @county= County.find(params[:county_id])
-        @school=@county.schools.create(school_params)
-        redirect_to county_path(@county)
+        @school=@county.schools.build(school_params)
+        if @school.save
+            redirect_to county_path(@county)
+        else
+            render 'new'
+        end
     end
     private
     def school_params
-        params.require(:school).permit(:name)
+        params.require(:school).permit(:name,:address)
     end
 end
