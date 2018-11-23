3e1bfc9d21eab6917c0d37af1e1d9683c20b3d77
Ana Moreno <amoreno5@uncc.edu>
2018-11-23 03:47:09 +0000

    Added validators in School and County model

--- app/models/county.rb
+++ app/models/county.rb
@@ -1,3 +1,5 @@
 class County < ApplicationRecord
-    has_many :schools
+    has_many :schools , dependent: :destroy
+    validates :name, presence: true, length: {minimum:5}
+    
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,8 @@
 class School < ApplicationRecord
   belongs_to :county
-  
+  validates :name, presence: true, length: {minimum:5}
+  validates :address, presence: true
+  validates :grade, presence: true, lenth: {minimum:4}
+  validates :phone, presence: true, format: {with: /\d{3}-\d{3}-\d{4}/, message: "please format xxx-xxx-xxxx"}
+  validates :website, presence: true, :format => URI::regexp(%w(http https))
 end
